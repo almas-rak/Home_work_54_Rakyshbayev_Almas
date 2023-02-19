8bca052d6b68df0675b51ec4c69c94fc6dc375e4
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-19 12:04:00 +0600

    тесты

--- source/my_market/views/test_views.py
+++ source/my_market/views/test_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render, redirect
 
 from my_market.forms import ImageForm
 
@@ -6,16 +7,13 @@ from my_market.forms import ImageForm
 # Create your views here.
 
 
-
-def image_upload_view(request):
-    """Process images uploaded by users"""
+def image_upload_view(request: WSGIRequest):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            # Get the current instance object to display in the template
             img_obj = form.instance
-            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+            return redirect(request, 'index.html', {'form': form, 'img_obj': img_obj})
     else:
         form = ImageForm()
     return render(request, 'test.html', {'form': form})
