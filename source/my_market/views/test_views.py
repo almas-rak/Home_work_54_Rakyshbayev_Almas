91b73d8817c5dd1048f45b886dbfd892a85d42bb
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-19 11:54:52 +0600

    тесты

---
+++ source/my_market/views/test_views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from my_market.forms import ImageForm
+
+
+# Create your views here.
+
+
+
+def image_upload_view(request):
+    """Process images uploaded by users"""
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current instance object to display in the template
+            img_obj = form.instance
+            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = ImageForm()
+    return render(request, 'test.html', {'form': form})
