2b7efbb92aa7f12dc3bbf472d2ff0c2dcf79f0f7
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-19 11:58:08 +0600

    добавление моделей в админку

--- source/my_market/admin.py
+++ source/my_market/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from my_market.models import Product, Category
+
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'price', 'image', 'category', 'add_date_time')
+    list_editable = ('product_name', 'price', 'image', 'category')
+    readonly_fields = ['preview']
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;>')
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name', 'description')
+    list_editable = ('category_name', 'description')
+
+
+admin.site.register(Category, CategoryProductAdmin)
