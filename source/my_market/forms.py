40972026d476aa764c26fdf9f08219b1d4acfb4c
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-19 21:09:30 +0600

    смена тестовой формы продуктов на рабочую

--- source/my_market/forms.py
+++ source/my_market/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from my_market.models import Product
 
 
-class ImageForm(forms.ModelForm):
+class AddProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('product_name', 'price', 'image')
+        fields = ('product_name', 'description', 'price', 'category', 'image')
