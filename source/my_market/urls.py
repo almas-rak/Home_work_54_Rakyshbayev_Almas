ec11c7b60aa4d01a7284107768727f1d2494211d
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-20 21:40:07 +0600

    добавлена ссылка на add_category

--- source/my_market/urls.py
+++ source/my_market/urls.py
@@ -5,10 +5,12 @@ from django.urls import path
 from my_market.views.base import index_view
 from my_market.views.add_view import add_product_view
 from my_market.views.detail_view_product import detail_view_product
+from my_market.views.add_category_product import add_category_product
 
 urlpatterns = [
     path('', index_view, name='home'),
     path('add/product/', add_product_view, name='add_product'),
+    path('add/category/', add_category_product, name='add_category'),
     path('view/product/<int:pk>', detail_view_product, name='detail_view_product')
 ]
 
