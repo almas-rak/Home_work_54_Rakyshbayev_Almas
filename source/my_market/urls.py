c1811815db487a656175a9bded868c509d029579
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-20 21:05:25 +0600

    добавление ссылки на детальное представление продукта, добавление имён ссылкам

--- source/my_market/urls.py
+++ source/my_market/urls.py
@@ -4,11 +4,12 @@ from django.urls import path
 
 from my_market.views.base import index_view
 from my_market.views.add_view import add_product_view
-
+from my_market.views.detail_view_product import detail_view_product
 
 urlpatterns = [
-    path('', index_view),
-    path('add/product/', add_product_view)
+    path('', index_view, name='home'),
+    path('add/product/', add_product_view, name='add_product'),
+    path('view/product/<int:pk>', detail_view_product, name='detail_view_product')
 ]
 
 if settings.DEBUG:
