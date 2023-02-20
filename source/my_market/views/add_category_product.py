87ceeebe623cc2bffc58f987a85625cf296e3e97
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-20 21:50:32 +0600

    исправлена ошибка в ссылке

--- source/my_market/views/add_category_product.py
+++ source/my_market/views/add_category_product.py
@@ -6,7 +6,7 @@ from my_market.forms import AddProductCategoryForm
 def add_category_product(request: WSGIRequest):
     if request.method == 'GET':
         form = AddProductCategoryForm()
-        return render(request, 'add_product.html', context={'form': form})
+        return render(request, 'add_product_category.html', context={'form': form})
     else:
         form = AddProductCategoryForm(request.POST, request.FILES)
         if form.is_valid():
