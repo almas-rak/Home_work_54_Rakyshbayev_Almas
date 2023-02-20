10d50ea0eada146561e23ee07b707d799ea0c5dc
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-20 21:13:42 +0600

    добавлена форма модели Category

--- source/my_market/forms.py
+++ source/my_market/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from my_market.models import Product
+from my_market.models import Product, Category
 
 
 class AddProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ('product_name', 'description', 'price', 'category', 'image')
+
+
+class AddProductCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('category_name', 'description')
